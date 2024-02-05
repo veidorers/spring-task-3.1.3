f75922ab5459e762184ad9476f038425fdd62915
Sergey Fomenko <artem.work749@gmail.com>
2024-02-05 17:27:21 +0300

    Display ROLE-NAME instead of ROLE_ROLE-NAME

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.Role;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,11 +17,11 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if(roles.contains("ROLE_USER") && roles.contains("ROLE_ADMIN")) {
+        if(roles.contains(Role.ROLE_USER.getTitle()) && roles.contains(Role.ROLE_ADMIN.getTitle())) {
           httpServletResponse.sendRedirect("/");
-        } else if (roles.contains("ROLE_USER")) {
+        } else if (roles.contains(Role.ROLE_USER.getTitle())) {
             httpServletResponse.sendRedirect("/user");
-        } else if(roles.contains("ROLE_ADMIN")) {
+        } else if(roles.contains(Role.ROLE_ADMIN.getTitle())) {
             httpServletResponse.sendRedirect("/admin/");
         } else {
             httpServletResponse.sendRedirect("/");
